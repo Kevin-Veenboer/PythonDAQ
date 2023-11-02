ef8240211f093e7a373f8c4b35bc7e41c8e081f2
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-02 10:23:45 +0100

    Added teh CSV saving and plotting of results

--- Experiment.py
+++ Experiment.py
@@ -1,6 +1,22 @@
 import pyvisa
 import time
 import matplotlib.pyplot as plt
+import pandas as pd
+
+# Deining storage path for data
+path = "C:/Users/12604275/Desktop/ECPC/DataStore/"
+
+# Defining conversion functions
+def convert_Analog_Digital(value):
+    steps = 1023
+    step_value = 3.3 / steps
+    return round(float(value) / step_value)
+
+
+def convert_Digital_Analog(value):
+    steps = 1023
+    step_value = 3.3 / steps
+    return round(float(value) * step_value, 2)
 
 
 rm = pyvisa.ResourceManager("@py")
@@ -14,18 +30,37 @@ Output_values = list(range(0, 1024))
 MeasurementsT = list()
 MeasurementsR = list()
 MeasurementsL = list()
+MeasurementsI = list()
+
 for val in Output_values:
     # Setting value of Arduino then measuring
     device.query(f"OUT:CH0 {val}")
-    Resistor_Voltage = float(device.query("MEAS:CH2?"))
-    Total_Voltage = float(device.query("MEAS:CH1?"))
+    Resistor_Voltage = convert_Digital_Analog(float(device.query("MEAS:CH2?")))
+    Total_Voltage = convert_Digital_Analog(float(device.query("MEAS:CH1?")))
+    LED_Voltage = round(Total_Voltage - Resistor_Voltage, 2)
 
     # Storing the measurements in lists
     MeasurementsT.append(Total_Voltage)
     MeasurementsR.append(Resistor_Voltage)
-    MeasurementsL.append(Total_Voltage - Resistor_Voltage)
+    MeasurementsL.append(LED_Voltage)
+
+    # Current is in mA !
+    MeasurementsI.append(round(1000 * (Resistor_Voltage / 220), 2))
 
     # Debug print
     print(
-        f"Total: {Total_Voltage}, Resistor: {Resistor_Voltage}, LED: {Total_Voltage-Resistor_Voltage}"
+        f"Total: {Total_Voltage}, Resistor: {Resistor_Voltage}, LED: {LED_Voltage}, Current(mV): {round(1000 * (Resistor_Voltage / 220), 2)}"
     )
+
+DF_dict = {
+    "Total Voltage (V)": MeasurementsT,
+    "Resistor Voltage (V)": MeasurementsR,
+    "LED Voltage (V)": MeasurementsL,
+    "Current (mA)": MeasurementsI,
+}
+# storing the data as CSV
+pd.DataFrame(DF_dict).to_csv(path + "ExperimentData.csv")
+
+# plotting the result
+plt.plot(MeasurementsL, MeasurementsI, marker="o", linestyle="None")
+plt.show()
