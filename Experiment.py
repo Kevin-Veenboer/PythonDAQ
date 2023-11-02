4c76164d43ff992b231f61c86179b950cc801892
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-02 12:34:04 +0100

    Added Device class and changed measurement code

--- Experiment.py
+++ Experiment.py
@@ -3,8 +3,12 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Deining storage path for data
-path = "C:/Users/12604275/Desktop/ECPC/DataStore/"
+# function to displa devices
+def list_devices():
+    rm = pyvisa.ResourceManager("@py")
+    ports = rm.list_resources()
+    return ports
+
 
 # Defining conversion functions
 def convert_Analog_Digital(value):
@@ -19,13 +23,51 @@ def convert_Digital_Analog(value):
     return round(float(value) * step_value, 2)
 
 
-rm = pyvisa.ResourceManager("@py")
-ports = rm.list_resources()
+class ArduinoVISADevice:
+    def __init__(self, port) -> None:
+        # Make connection with device
+        self.rm = pyvisa.ResourceManager("@py")
+        self.device = self.rm.open_resource(
+            port, read_termination="\r\n", write_termination="\n"
+        )
 
-device = rm.open_resource(
-    "ASRL5::INSTR", read_termination="\r\n", write_termination="\n"
-)
+        # Initialize the channel values
+        self.CH0_value = None
+        self.CH1_value = None
+        self.CH2_value = None
 
+    def get_identification(self) -> None:
+        print(self.device.query("*IDN?"))
+
+    # Method to set output values (0-1023 input expected)
+    def set_output_value(self, value) -> None:
+        self.device.query(f"OUT:CH0 {value}")
+
+    # functions to get output value (CH0) or input values (CH1 or CH2)
+    def get_output_value(self) -> int:
+        # if no value is set then return 0
+        if self.CH0_value == None:
+            return 0
+        return self.CH0_value
+
+    def get_input_value(self, channel) -> int:
+        if channel not in [1, 2]:
+            print("Available channels are 1 and 2!")
+        elif channel == 1:
+            return int(device.query("MEAS:CH1?"))
+        else:
+            return int(device.query("MEAS:CH1?"))
+
+
+# Defining storage path for data
+path = "C:/Users/12604275/Desktop/ECPC/DataStore/"
+
+
+port = list_devices[0]
+
+Device = ArduinoVISADevice(port=port)
+
+# Intitializing the measurement lists
 Output_values = list(range(0, 1024))
 MeasurementsT = list()
 MeasurementsR = list()
@@ -34,9 +76,9 @@ MeasurementsI = list()
 
 for val in Output_values:
     # Setting value of Arduino then measuring
-    device.query(f"OUT:CH0 {val}")
-    Resistor_Voltage = convert_Digital_Analog(float(device.query("MEAS:CH2?")))
-    Total_Voltage = convert_Digital_Analog(float(device.query("MEAS:CH1?")))
+    Device.set_output_value(value=val)
+    Resistor_Voltage = convert_Digital_Analog(float(Device.get_input_value(channel=2)))
+    Total_Voltage = convert_Digital_Analog(float(Device.get_input_value(channel=1)))
     LED_Voltage = round(Total_Voltage - Resistor_Voltage, 2)
 
     # Storing the measurements in lists
