a4b5639aed40bee34ac9bf93a87f68930e079b60
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-09 10:32:15 +0100

    variable names to be in line with grading (model)

--- diode_experiment.py
+++ diode_experiment.py
@@ -4,73 +4,83 @@ import numpy as np
 
 class DiodeExperiment:
     def __init__(self) -> None:
-        # TA said I did not have to use resistor error
-        self.Resistor_Loads = list()
-        self.Total_Voltages = list()
-        self.Total_Voltages_Error = list()
-        self.Resistor_Voltages = list()
-        self.Resistor_Voltages_Error = list()
-        self.LED_Voltages = list()
-        self.LED_Voltages_Error = list()
-        self.Currents = list()
-        self.Currents_Error = list()
+        # TA said I did not have to use resistor errors
+        self.resistor_loads = []
+        self.total_voltages = []
+        self.total_voltages_errors = []
+        self.resistor_voltages = []
+        self.resistor_voltages_errors = []
+        self.led_voltages = []
+        self.led_voltages_errors = []
+        self.currents = []
+        self.currents_errors = []
 
     # Method to start an experiment
-    def scan(self, device_range=1024, Resistor_Load=220, sample_size=5) -> tuple:
+    def scan(self, device_range=1024, resistor_load=220, sample_size=5) -> tuple:
         # Make sure to clear the old results first
         self.clear()
 
         port = list_devices()[0]
-        Device = ArduinoVISADevice(port=port)
+        device = ArduinoVISADevice(port=port)
 
         for output_val in range(0, device_range):
-            Device.set_output_value(value=output_val)
+            device.set_output_value(value=output_val)
 
-            Measurement_T_U = list()
-            Measurement_R_U = list()
+            measurement_total_volt = []
+            measurement_resistor_volt = []
             # Take measurements (take a few to determine the error)
             for _ in range(sample_size):
-                Measurement_T_U.append(Device.get_input_value(channel=1))
-                Measurement_R_U.append(Device.get_input_value(channel=2))
+                measurement_total_volt.append(device.get_input_value(channel=1))
+                measurement_resistor_volt.append(device.get_input_value(channel=2))
 
             # Calculate the total volt and resister volt
-            Total_U = np.mean(Measurement_T_U)
-            Resistor_U = np.mean(Measurement_R_U)
+            total_volt = np.mean(measurement_total_volt)
+            resistor_volt = np.mean(measurement_resistor_volt)
 
-            # Note standard error > Standard Deviation / sqrt(sample_size)
-            T_U_Err = np.std(Measurement_T_U) / np.sqrt(sample_size)
-            R_U_Err = np.std(Measurement_R_U) / np.sqrt(sample_size)
+            # Note standard error = Standard Deviation / sqrt(sample_size)
+            total_volt_error = np.std(measurement_total_volt) / np.sqrt(sample_size)
+            resistor_volt_error = np.std(measurement_resistor_volt) / np.sqrt(
+                sample_size
+            )
 
             # Add the measurement
-            self.add_Measurement(Resistor_Load, Total_U, Resistor_U, T_U_Err, R_U_Err)
+            self.add_measurement(
+                resistor_load,
+                total_volt,
+                resistor_volt,
+                total_volt_error,
+                resistor_volt_error,
+            )
 
         # Return the experiment data as tuple with headers and data
         return self.export_experiment_data()
 
     # Method to add measurement to instance
-    def add_Measurement(self, R, Total_U, Resistor_U, T_U_Err, R_U_Err) -> None:
+    def add_measurement(
+        self, R, total_volt, resistor_volt, total_volt_error, resistor_volt_error
+    ) -> None:
         assert R != 0, "Loads of zero are not allowed"
 
         # Append the values that do not need calculation
-        self.Resistor_Loads.append(R)
-        self.Total_Voltages.append(Total_U)
-        self.Total_Voltages_Error.append(T_U_Err)
-        self.Resistor_Voltages.append(Resistor_U)
-        self.Resistor_Voltages_Error.append(R_U_Err)
+        self.resistor_loads.append(R)
+        self.total_voltages.append(total_volt)
+        self.total_voltages_errors.append(total_volt_error)
+        self.resistor_voltages.append(resistor_volt)
+        self.resistor_voltages_errors.append(resistor_volt_error)
 
         # Calculate the current with voltage/load and store the result
-        circuit_current = Resistor_U / R
-        current_err = R_U_Err / R
-        self.Currents.append(circuit_current)
-        self.Currents_Error.append(current_err)
+        circuit_current = resistor_volt / R
+        current_error = resistor_volt_error / R
+        self.currents.append(circuit_current)
+        self.currents_errors.append(current_error)
 
         # Calculate LED voltage and load and power
-        LED_U = Total_U - Resistor_U
-        LED_U_err = np.sqrt((T_U_Err) ** 2 + (R_U_Err) ** 2)
+        led_volt = total_volt - resistor_volt
+        led_volt_error = np.sqrt((total_volt_error) ** 2 + (resistor_volt_error) ** 2)
 
         # Store results
-        self.LED_Voltages.append(LED_U)
-        self.LED_Voltages_Error.append(LED_U_err)
+        self.led_voltages.append(led_volt)
+        self.led_voltages_errors.append(led_volt_error)
 
     def export_experiment_data(self) -> tuple:
         headers = [
@@ -85,26 +95,26 @@ class DiodeExperiment:
             "Resistor load (Ohm)",
         ]
         data = zip(
-            self.Total_Voltages,
-            self.Total_Voltages_Error,
-            self.Resistor_Voltages,
-            self.Resistor_Voltages_Error,
-            self.LED_Voltages,
-            self.LED_Voltages_Error,
-            self.Currents,
-            self.Currents_Error,
-            self.Resistor_Loads,
+            self.total_voltages,
+            self.total_voltages_errors,
+            self.resistor_voltages,
+            self.resistor_voltages_errors,
+            self.led_voltages,
+            self.led_voltages_errors,
+            self.currents,
+            self.currents_errors,
+            self.resistor_loads,
         )
-        print(self.LED_Voltages)
+
         return (headers, data)
 
     def clear(self):
-        self.Resistor_Loads = list()
-        self.Total_Voltages = list()
-        self.Total_Voltages_Error = list()
-        self.Resistor_Voltages = list()
-        self.Resistor_Voltages_Error = list()
-        self.LED_Voltages = list()
-        self.LED_Voltages_Error = list()
-        self.Currents = list()
-        self.Currents_Error = list()
+        self.resistor_loads = []
+        self.total_voltages = []
+        self.total_voltages_errors = []
+        self.resistor_voltages = []
+        self.resistor_voltages_errors = []
+        self.led_voltages = []
+        self.led_voltages_errors = []
+        self.currents = []
+        self.currents_errors = []
