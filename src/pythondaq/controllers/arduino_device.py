921aca9e3d5476398e44d40bc6a6191bb0e18a2d
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-23 09:46:22 +0100

    added docstrings to controller

--- src/pythondaq/controllers/arduino_device.py
+++ src/pythondaq/controllers/arduino_device.py
@@ -2,28 +2,37 @@ import pyvisa
 
 # Function to display devices
 def list_devices():
+    """This function shows which devices are connected
+
+    Returns:
+        list: a list containing strings of the ports from the connected devices
+    """
     rm = pyvisa.ResourceManager("@py")
     ports = rm.list_resources()
     return ports
 
 
 class ArduinoVISADevice:
+    """This class allows users to manage their arduino experiment controller"""
+
     def __init__(self, port) -> None:
+        """Creates an instance of the ArduinoVISADevice class and connect to the controller
+
+        Args:
+            port (string): the port of the device to connect with
+        """
         # Make connection with device
         self.rm = pyvisa.ResourceManager("@py")
         self.device = self.rm.open_resource(
             port, read_termination="\r\n", write_termination="\n"
         )
 
-        # Initialize the channel values
-        self.channel_0_value = None
-        self.channel_1_value = None
-        self.channel_2_value = None
-
     # Method to get device identification string
     def get_identification(self) -> None:
+        """Prints the identification string of the device"""
         try:
             print(self.device.query("*IDN?"))
+        # If the request is made to a device which does not support this query then handle the error
         except pyvisa.errors.VisaIOError:
             print(
                 "The device at the given port does not respond to this query please try anoher port"
@@ -31,35 +40,72 @@ class ArduinoVISADevice:
 
     # Defining conversion functions
     def convert_analog_digital(self, value):
+        """Converts an analog value to digital value
+
+        Args:
+            value (float): the analog value to be converted
+
+        Returns:
+            int: digital value between 0 - 1023
+        """
         steps = 1023
         step_value = 3.3 / steps
         return round(float(value) / step_value)
 
     def convert_digital_analog(self, value):
+        """Converts a digital value to an analog value
+
+        Args:
+            value (int): the digital value to be converted
+
+        Returns:
+            float: analog value between 0.0-3.3
+        """
         steps = 1023
         step_value = 3.3 / steps
         return round(float(value) * step_value, 2)
 
-    # Method to set output values (0-1023 input expected)
     def set_output_value(self, value) -> None:
+        """This function sets the value of the output channel on the device
+
+        Args:
+            value (int): value to set the output channel to, ranging from 0-1023
+        """
         self.device.query(f"OUT:CH0 {value}")
 
-    # Functions to get output value (CH0) or input values (CH1 or CH2)
     def get_output_value(self) -> int:
-        # if no value is set then return 0
+        """This function reads the current value on the output channel
+
+        Returns:
+            int: current value of the output channel
+        """
         return self.device.query(f"OUT:CH0?")
 
-    # Function to get the value on channel 1 or 2 of the device as a digital value
     def get_input_value(self, channel) -> float:
+        """This function measures the digital values on the input channels
+
+        Args:
+            channel (int): the channel from which the value has to be read
+
+        Returns:
+            int: digital value on the measured input channel
+        """
         if channel not in [1, 2]:
             print("Available channels are 1 and 2!")
         elif channel == 1:
-            return float(self.device.query("MEAS:CH1?"))
+            return int(self.device.query("MEAS:CH1?"))
         else:
-            return float(self.device.query("MEAS:CH1?"))
+            return int(self.device.query("MEAS:CH1?"))
 
-    # Function to get the value on channel 1 or 2 of the device as an analog value
     def get_input_voltage(self, channel) -> float:
+        """This function measures the digital values on the input channels
+
+        Args:
+            channel (int): the channel from which the value has to be read
+
+        Returns:
+            float: analog value on the measured input channel
+        """
         if channel not in [1, 2]:
             print("Available channels are 1 and 2!")
         if channel == 1:
